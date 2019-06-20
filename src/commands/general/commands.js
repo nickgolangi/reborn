eb977d43447b65aa3a29e396d69b880bc7a03386
Asshley <ashwafaw@gmail.com>
2019-06-20 15:40:42 -0400

    feat: commands

---
+++ src/commands/general/commands.js
@@ -0,0 +1,57 @@
+/**
+ * Reborn - The core control of the only truly free and fair discord server.
+ * Copyright (C) 2019  John Boyer
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+const { Command } = require('patron.js');
+const registry = require('../../services/registry.js');
+
+module.exports = new class Commands extends Command {
+  constructor() {
+    super({
+      description: 'View the current commands.',
+      groupName: 'general',
+      names: ['commands', 'cmds']
+    });
+  }
+
+  async run(msg) {
+    const { groups } = registry;
+    const embed = {
+      title: 'The current commands',
+      fields: []
+    };
+
+    for (let i = 0; i < groups.length; i++) {
+      embed.fields.push({
+        name: groups[i].name, value: '', inline: true
+      });
+
+      for (let j = 0; j < groups[i].commands.length; j++) {
+        const command = groups[i].commands[j];
+
+        embed.fields[embed.fields.length - 1].value += command.names[0];
+
+        if (j + 1 !== groups[i].commands.length - 1) {
+          embed.fields[embed.fields.length - 1].value += ', ';
+        }
+
+        if (j === groups[i].commands.length - 1) {
+          embed.fields[embed.fields.length - 1].value += 'and ';
+        }
+      }
+    }
+
+    await msg.channel.createMessage({ embed });
+  }
+}();
