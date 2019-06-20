4dca4172230db693a740a335351c94d44b670cf5
Asshley <ashwafaw@gmail.com>
2019-06-20 14:33:45 -0400

    fix: judge_creator prec

--- src/preconditions/command/judge_creator.js
+++ src/preconditions/command/judge_creator.js
@@ -22,7 +22,13 @@ module.exports = new class JudgeCreator extends Precondition {
   }
 
   async run(cmd, msg) {
-    const { judge_id } = db.get_channel_case(msg.channel.id);
+    const c_case = db.get_channel_case(msg.channel.id);
+
+    if (!c_case) {
+      return PreconditionResult.fromError(cmd, 'This channel case doesn\'t exist.');
+    }
+
+    const { judge_id } = c_case;
     const judge = msg.channel.guild.members.get(judge_id);
 
     if (msg.author.id !== judge_id && judge) {
