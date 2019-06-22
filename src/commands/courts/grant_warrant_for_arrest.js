18347b8c0656a7bdb5820635a6d7a85b3d0e9857
Ash <28841569+Asshley@users.noreply.github.com>
2019-06-21 23:37:22 -0400

    Remove warrant channel stuff

--- src/commands/courts/grant_warrant_for_arrest.js
+++ src/commands/courts/grant_warrant_for_arrest.js
@@ -44,12 +44,6 @@ module.exports = new class GrantWarrantForArrest extends Command {
   }
 
   async run(msg, args) {
-    const { warrant_channel } = db.fetch('guilds', { guild_id: msg.channel.guild.id });
-
-    if (!warrant_channel) {
-      return CommandResult.fromError('The warrant channel needs to be set up.');
-    }
-
     const verified = await discord.verify_msg(
       msg, `**${discord.tag(msg.author)}**, **Warning:** Handing out false warrants will result \
 in impeachment. Type \`I'm sure\` if you are sure you want to grant this warrant.`
